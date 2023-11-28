4c4085c87548cfd0d2e61336c0d1eadfb821d513
sachin-boral <sachinboral100@gmail.com>
2023-11-28 18:05:06 +0530

    2nd model class

--- CurrencyConverter/src/main/java/com/currencyConverter/model/CurrencyConversionModel.java
+++ CurrencyConverter/src/main/java/com/currencyConverter/model/CurrencyConversionModel.java
@@ -2,4 +2,23 @@ package com.currencyConverter.model;
 
 public class CurrencyConversionModel {
 
+	private String fromCurrency;
+	private String toCurrency;
+
+	public String getFromCurrency() {
+		return fromCurrency;
+	}
+
+	public void setFromCurrency(String fromCurrency) {
+		this.fromCurrency = fromCurrency;
+	}
+
+	public String getToCurrency() {
+		return toCurrency;
+	}
+
+	public void setToCurrency(String toCurrency) {
+		this.toCurrency = toCurrency;
+	}
+
 }
